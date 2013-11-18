af58ec21ac00cb4e111ec6ccc657a34ec9abcc03
Ashish Bista <poisonta03@gmail.com>
2013-11-18 18:04:37 +0545

    Copying env vars in the shared application.yml file

--- opsworks-env-vars/recipes/default.rb
+++ opsworks-env-vars/recipes/default.rb
@@ -2,7 +2,7 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
-  template "#{deploy[:deploy_to]}/current/config/application.yml" do
+  template "#{deploy[:deploy_to]}/shared/config/application.yml" do
     cookbook 'opsworks-env-vars'
     mode "0660"
     group deploy[:group]
