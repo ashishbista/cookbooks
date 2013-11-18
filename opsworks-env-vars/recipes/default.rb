46afae654d7117fed337f0fc8e9426e2ae93caac
Ashish Bista <poisonta03@gmail.com>
2013-11-18 15:01:17 +0545

    Fixed config directory name

--- opsworks-env-vars/recipes/default.rb
+++ opsworks-env-vars/recipes/default.rb
@@ -3,7 +3,7 @@ include_recipe "deploy"
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  template "#{deploy[:deploy_to]}/config/application.yml" do
+  template "#{deploy[:deploy_to]}/current/config/application.yml" do
     cookbook 'opsworks-env-vars'
     mode "0660"
     group deploy[:group]
