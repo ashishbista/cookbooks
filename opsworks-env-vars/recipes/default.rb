538d634c221a484038b992a88e5a8aab6c8599ab
Ashish Bista <poisonta03@gmail.com>
2013-11-18 16:17:14 +0545

    Fixed application path issue

--- opsworks-env-vars/recipes/default.rb
+++ opsworks-env-vars/recipes/default.rb
@@ -2,12 +2,11 @@ include_recipe "deploy"
 
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
-
-  template "/srv/www/#{application}/current/config/application.yml" do
+  template "#{deploy[:deploy_to]}/current/config/application.yml" do
     cookbook 'opsworks-env-vars'
     mode "0660"
     group deploy[:group]
     owner deploy[:user]
-    variables(:environment_variables => node[:deploy][:environment_variables])
+    variables(:environment_variables => node[:environment_variables])
   end
 end
